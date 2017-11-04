b66c2c8dca2fe672e46f738d54408c27393ba47c
Samuel Vieira <vieira.samuel@outlook.com.br>
2017-11-03 23:48:48 -0300

    Remoção de bugs

--- TVMaster/src/view/TelaLogin.java
+++ TVMaster/src/view/TelaLogin.java
@@ -6,6 +6,8 @@
 
 package view;
 
+import java.awt.event.ActionEvent;
+
 /**
  *
  * @author Samuel Vieira
@@ -145,11 +147,13 @@ public class TelaLogin extends javax.swing.JFrame {
                 .addContainerGap(170, Short.MAX_VALUE))
         );
 
+        jPanel2.getAccessibleContext().setAccessibleName("");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 900, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 952, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -189,7 +193,7 @@ public class TelaLogin extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
